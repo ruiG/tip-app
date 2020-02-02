e950adcfdd42d4aacba1d3ee9267a83d75e3ae9b
ruiG <rui.nf1@gmail.com>
2020-02-02 18:21:07 +0100

    Form state, country select, Review component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import * as R from "rambda";
 import countriesData from "../../config/data.json";
+import Review from "../Review";
 
 function Form() {
   const [step, setStep] = useState(0);
@@ -15,24 +16,55 @@ function Form() {
   };
 
   const setCountry = countryCode => {
+    if (step < 1) {
+      setStep(1);
+    }
     setForm({ ...form, countryCode });
+  };
+
+  const setBill = bill => {
+    if (step < 2) {
+      setStep(2);
+    }
+    setForm({ ...form, bill });
   };
 
   return (
     <div className="form">
       <h1>Where are you?</h1>
-      <CountrySelect
-        countries={countriesData}
-        selectedCountry={form.countryCode}
-        onSetCountry={setCountry}
-      />
+      {step >= 0 ? (
+        <CountrySelect
+          countries={countriesData}
+          selectedCountry={form.countryCode}
+          onSetCountry={setCountry}
+        />
+      ) : null}
+      {step >= 1 ? <Bill bill={form.bill} onSetBill={setBill} /> : null}
+      {step >= 2 ? <Review /> : null}
     </div>
   );
 }
 
+const Bill = ({ bill, onSetBill }) => {
+  const handleSetBill = event => {
+    console.log(event);
+    onSetBill(event.target.value);
+  };
+
+  return (
+    <div>
+      <input onChange={handleSetBill} type="tel" pattern="[0-9]*" novalidate />
+    </div>
+  );
+};
+
+const Info = () => <div>Info</div>;
+
 const CountrySelect = ({ countries, selectedCountry, onSetCountry }) => {
   const countryOption = ({ countryKey, name }) => (
-    <option key={countryKey} value={countryKey}>{name}</option>
+    <option key={countryKey} value={countryKey}>
+      {name}
+    </option>
   );
 
   const handleSetCountry = event => {
@@ -42,7 +74,11 @@ const CountrySelect = ({ countries, selectedCountry, onSetCountry }) => {
   const countryOptions = R.map(countryOption, R.values(countries));
 
   return (
-    <select id="countrySelect" onChange={handleSetCountry} value={selectedCountry}>
+    <select
+      id="countrySelect"
+      onChange={handleSetCountry}
+      value={selectedCountry}
+    >
       <option value="">Select a Country</option>
       {countryOptions}
     </select>
