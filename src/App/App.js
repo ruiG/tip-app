d18fbb21e26b1fcbdb8908dccd5cc775cb816511
paulmorar <paulcornelmorar@gmail.com>
2020-02-02 20:35:22 +0100

    Add text

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Form from "../components/Form";
+import Button from "../components/Button";
 import "./App.scss";
 import logo from "../images/tipsy-logo.png";
 
@@ -16,13 +17,7 @@ function App() {
         <img src={logo} className="tipsy__logo__img" alt="Tipsy" />
       </div>
 
-      {activeForm ? (
-        <Form />
-      ) : (
-        <button className="tipsy__start" onClick={handleStart}>
-          Start
-        </button>
-      )}
+      {activeForm ? <Form /> : <Button onClick={handleStart} label={"Start"} />}
     </div>
   );
 }
