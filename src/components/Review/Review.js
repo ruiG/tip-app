e70f830c423a545783cddd78b9ab462a1a5fc52d
ruiG <rui.nf1@gmail.com>
2020-02-02 18:24:40 +0100

    fix jsx html

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -3,19 +3,19 @@ import './Review.scss'
 
 const Review = () => {
   return (
-    <div class="container">
-      <div class="feedback">
-        <div class="rating">
+    <div className="container">
+      <div className="feedback">
+        <div className="rating">
           <input type="radio" name="rating" id="rating-5" />
-          <label for="rating-5"></label>
+          <label htmlFor="rating-5"></label>
           <input type="radio" name="rating" id="rating-4" />
-          <label for="rating-4"></label>
+          <label htmlFor="rating-4"></label>
           <input type="radio" name="rating" id="rating-3" />
-          <label for="rating-3"></label>
+          <label htmlFor="rating-3"></label>
           <input type="radio" name="rating" id="rating-2" />
-          <label for="rating-2"></label>
+          <label htmlFor="rating-2"></label>
           <input type="radio" name="rating" id="rating-1" />
-          <label for="rating-1"></label>
+          <label htmlFor="rating-1"></label>
         </div>
       </div>
     </div>
