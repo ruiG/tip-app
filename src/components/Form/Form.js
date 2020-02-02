a37f796498e4dd091452596e8ea101609cc48275
ruiG <rui.nf1@gmail.com>
2020-02-02 15:42:00 +0100

    Country select

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
+import * as R from "rambda";
+import countriesData from "../../config/data.json";
 
 function Form() {
-  const [activeForm, setActiveForm] = useState(false);
+  const [step, setStep] = useState(0);
 
-  const handleStart = () => {
-    setActiveForm(true);
+  const handleNextState = step => {
+    setStep(step);
   };
 
-  return <div className="form"></div>;
+  return (
+    <div className="form">
+      <h1>Where are you?</h1>
+      <CountrySelect countries={countriesData} />
+    </div>
+  );
 }
+
+const CountrySelect = ({ countries }) => {
+  const countryOption = ({ countryKey, name }) => (
+    <option value={countryKey}>{name}</option>
+  );
+
+  const countryOptions = R.map(countryOption, R.values(countries));
+
+  console.log(countryOptions);
+  return (
+    <select>
+      <option value="">Select a Country</option>
+      {countryOptions}
+    </select>
+  );
+};
 
 export default Form;
